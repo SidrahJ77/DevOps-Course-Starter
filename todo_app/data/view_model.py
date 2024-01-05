4a4a9424a76482c8d1c6b873e767c08e4ab03f3c
Sidrah Javed <sidrah.javed@asda.co.uk>
2024-01-05 15:39:38 +0000

    Add done_items property via TDD

--- todo_app/data/view_model.py
+++ todo_app/data/view_model.py
@@ -6,3 +6,19 @@ class ViewModel:
     @property
     def items(self):
         return self._items
+    
+    @property
+    def todo_items(self):
+        return []
+    
+    @property
+    def doing_items(self):
+        return []
+    
+    @property
+    def done_items(self) -> list[Item]:
+        output = []
+        for item in self._items:
+            if item.status == "Done":
+                output.append(item)
+                return output
