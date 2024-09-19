8b3a1709925dff42788caaed62d10ca28a966106
SidrahJ77 Jay <sidrah.javed@asda.co.uk>
2024-09-19 16:18:54 +0000

    Remove references to Trello and update documentation

--- todo_app/data/item.py
+++ todo_app/data/item.py
@@ -3,11 +3,6 @@ class Item:
         self.id = id
         self.name = name
         self.status = status
-
-    @classmethod
-    def from_trello_card(cls, card, list):
-        return Item(card['id'], card['name'], list['name'])
-    
     
     @classmethod
     def from_mongo_document(cls, document):
